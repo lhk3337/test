a285a9b218c6ec9b3106f274fc22f761430963aa
lhk3337 <lhk3337@naver.com>
2021-07-20 18:23:09 +0900

    함수 분리

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("root");
+
 let ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWSURL = "https://api.hnpwa.com/v0/news/1.json";
@@ -10,32 +11,35 @@ const getData = (url) => {
   return JSON.parse(ajax.response);
 };
 
-const newsFeed = getData(NEWSURL);
-const ul = document.createElement("ul");
+const newsFeed = () => {
+  const newsList = [];
+  const newsFeed = getData(NEWSURL);
+  newsList.push("<ul>");
 
-window.addEventListener("hashchange", () => {
+  for (let i = 0; i < newsFeed.length; i++) {
+    // 메인 화면 리스트
+    newsList.push(`
+    <li>
+      <a href = "#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+    `);
+  }
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
+};
+
+const newsDetail = () => {
   const id = location.hash.substr(1);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
-    <div><a href="#">목록으로</a></div>
+    <div><a href="/">목록으로</a></div>
   `;
-});
+};
 
+window.addEventListener("hashchange", newsDetail);
 
-const newsList = [];
-newsList.push('<ul>');
-
-for (let i = 0; i < newsFeed.length; i++) {
-  // 메인 화면 리스트
-  newsList.push(`
-  <li>
-    <a href = "#${newsFeed[i].id}">
-      ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>
-  `)
-}
-newsList.push("</ul>")
-container.innerHTML=newsList.join('');
+newsFeed();
