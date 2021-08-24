e05825cccbf43a7f50d553017dd223ad8b70fc1e
lhk3337 <lhk3337@gmail.com>
2021-08-24 17:19:32 +0900

    div content del

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,7 +1,6 @@
 const container = document.getElementById("root");
 
 let ajax = new XMLHttpRequest();
-const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/$id.json";
 
